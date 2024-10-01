c3d38ddd7e0c4bdd3927c13fd96945922dd608ec
roselyne mwaniki <roselynewairimu30@gmail.com>
2024-10-01 10:59:11 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,16 @@
+// Function expression for divide
+const divide = function(a, b) {
+    return a / b;
+};
+
+// Arrow function for square
+const square = (x) => {
+    return x * x;
+};
+
+// Arrow function for add
+const add = (a, b) => {
+    return a + b;
+};
+
+
